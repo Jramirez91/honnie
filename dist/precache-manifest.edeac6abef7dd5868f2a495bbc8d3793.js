ebf7a1d463dda5c7e1701732259ab20b304fbe40
Jimmy Ramirez <jbox@jbox.local>
2019-04-08 23:26:45 -0500

    Publicación del sitio

---
+++ dist/precache-manifest.edeac6abef7dd5868f2a495bbc8d3793.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "71e8efe417b8ecba0ff61bdd9f00c717",
+    "url": "/img/5.71e8efe4.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "ed5bccfa5b6553c2733fcab615e1ded5",
+    "url": "/img/8.ed5bccfa.jpg"
+  },
+  {
+    "revision": "3f35c274f58d1faa21f7",
+    "url": "/js/chunk-vendors.173a0489.js"
+  },
+  {
+    "revision": "17fc141f2b347a3bcb59ea69a62cc26e",
+    "url": "/img/2.17fc141f.jpg"
+  },
+  {
+    "revision": "58421cf004c14b0fbadca17d4e147d08",
+    "url": "/img/6.58421cf0.jpg"
+  },
+  {
+    "revision": "db38afa853833da4035643f32fc7e046",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6a72c26234617c7487b3d654f15063b",
+    "url": "/img/9.c6a72c26.jpg"
+  },
+  {
+    "revision": "72c3ae1b582a51c18a0a23d46d86f851",
+    "url": "/img/6.72c3ae1b.jpg"
+  },
+  {
+    "revision": "35839b35a6d4c6f5d4b8",
+    "url": "/js/app.c1644c0e.js"
+  },
+  {
+    "revision": "5d23932f5837e1d9342cc84bd1c430b5",
+    "url": "/img/7.5d23932f.jpg"
+  },
+  {
+    "revision": "1bd8f2a104dc9e7578bd9f7774234f01",
+    "url": "/img/6.1bd8f2a1.jpg"
+  },
+  {
+    "revision": "5942c589552683606a3f227c42e8e132",
+    "url": "/img/5.5942c589.jpg"
+  },
+  {
+    "revision": "a0090841cf29d9915996e00d1e338246",
+    "url": "/img/4.a0090841.jpg"
+  },
+  {
+    "revision": "6da449ac9abffd95d9619d5580ffbc01",
+    "url": "/img/4.6da449ac.jpg"
+  },
+  {
+    "revision": "0008d56a54d93ed1381e89f2a4da4b53",
+    "url": "/img/3.0008d56a.jpg"
+  },
+  {
+    "revision": "9e913147e034c0a2e4c3df68931e4a6e",
+    "url": "/img/2.9e913147.jpg"
+  },
+  {
+    "revision": "f367a1d7732257af2f7a73a54efd0cb1",
+    "url": "/img/1.f367a1d7.jpg"
+  },
+  {
+    "revision": "35839b35a6d4c6f5d4b8",
+    "url": "/css/app.17ec17af.css"
+  }
+];
